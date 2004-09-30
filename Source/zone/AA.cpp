c1535863c1eb6c1add25be04584db620bc675db5
edgar1898 <edgar1898>
2004-09-30 02:49:59 +0000

    Automated Update

---
+++ Source/zone/AA.cpp
@@ -0,0 +1,187 @@
+#include "client.h"
+#include "StringIDs.h"
+
+void Client::SendAAStats() {
+	APPLAYER* outapp = new APPLAYER(OP_SendAAStats, sizeof(AltAdvStats_Struct));
+	AltAdvStats_Struct *aps = (AltAdvStats_Struct *)outapp->pBuffer;
+	aps->experience = m_pp.expAA;
+	aps->experience = (int32)(((float)330.0f * (float)m_pp.expAA) / (float)max_AAXP);
+	aps->unspent = m_pp.aapoints;
+	aps->percentage = m_pp.perAA;
+	QueuePacket(outapp);
+	safe_delete(outapp);
+}
+void Client::BuyAA(AA_Action* action){
+	SendAA_Struct* aa2 = zone->FindAA(action->ability);
+	if(!aa2){
+		for(int i=1;i<5;i++){
+			if(aa2 = zone->FindAA(action->ability-i))
+				i=4;
+		}
+	}
+	int32 cur_level = GetAA(aa2->id);
+	if(aa2 && m_pp.aapoints >= aa2->cost && cur_level < aa2->max_level){
+		SetAA(aa2->id,cur_level+1);
+		m_pp.aapoints -= aa2->cost;
+		database.SetPlayerAlternateAdv(account_id, m_pp.name, &aa);
+		SendAA(aa2->id);
+		SendAATable();
+		char val1[20]={0};
+		char val2[20]={0};
+		char val3[20]={0};
+		char points[20]={0};
+		char point[20]={0};
+		const char* points2=ConvertArray(AA_POINTS,points);
+		const char* point2=ConvertArray(AA_POINT,point);
+		if(cur_level<1){
+			if(aa2->cost>1)
+				Message_StringID(15,AA_GAIN_ABILITY,ConvertArray(aa2->title_sid,val1),ConvertArray(aa2->cost,val2),points2);
+			else
+				Message_StringID(15,AA_GAIN_ABILITY,ConvertArray(aa2->title_sid,val1),ConvertArray(aa2->cost,val2),point2);
+		}
+		else{
+			if(aa2->cost>1)
+				Message_StringID(15,AA_IMPROVE,ConvertArray(aa2->title_sid,val1),ConvertArray(cur_level,val2),ConvertArray(aa2->cost,val3),points2);
+			else
+				Message_StringID(15,AA_IMPROVE,ConvertArray(aa2->title_sid,val1),ConvertArray(cur_level,val2),ConvertArray(aa2->cost,val3),point2);
+		}
+		SendAAStats();
+	}
+}
+void Client::SendAATimer(UseAA_Struct *uaa){
+	APPLAYER* outapp = new APPLAYER(OP_AAAction,sizeof(UseAA_Struct));
+	UseAA_Struct* uaaout = (UseAA_Struct*)outapp->pBuffer;
+	uaaout->ability=uaa->ability;
+	uaaout->begin=uaa->begin;
+	uaaout->end=uaa->end;
+	QueuePacket(outapp);
+	safe_delete(outapp);
+}
+void Client::ActivateAA(int activate){
+	int32 timermod=0;
+	switch(activate){
+		case 50:
+			MemorizeSpell(8,2750,3);
+			CastToMob()->CastSpell(2750,this->GetID());
+			timermod=7200;
+			break;
+		case 51:
+			MemorizeSpell(8,2751,3);
+			CastToMob()->CastSpell(2751,target->GetID());
+			timermod=112;
+			break;
+		case 80:
+			MemorizeSpell(8,2765,3);
+			CastToMob()->CastSpell(2765,this->GetID());
+			timermod=7;
+			break;
+	}
+	time_t timestamp=time(NULL);
+	
+	APPLAYER* outapp = new APPLAYER(OP_AAAction,sizeof(UseAA_Struct));
+	UseAA_Struct* uaa = (UseAA_Struct*)outapp->pBuffer;
+	uaa->ability=activate;
+	uaa->begin=timestamp;
+	uaa->end=timestamp;
+	database.UpdateAATimers(this->CharacterID(),timestamp+timermod,timestamp,activate);
+	QueuePacket(outapp);
+	safe_delete(outapp);
+}
+void Client::SendAATable() {
+    APPLAYER* outapp = new APPLAYER(OP_RespondAA, sizeof(PlayerAA_Struct));
+    PlayerAA_Struct* aa2=(PlayerAA_Struct*)outapp->pBuffer;
+	for(int i=0;i<MAX_AAS;i++){
+		if(aa.aa_list[i].aa_value>1)
+			aa2->aa_list[i].aa_skill=aa.aa_list[i].aa_skill+aa.aa_list[i].aa_value-1;
+		else
+			aa2->aa_list[i].aa_skill=aa.aa_list[i].aa_skill;
+		aa2->aa_list[i].aa_value=aa.aa_list[i].aa_value;
+	}
+	outapp->Deflate();
+    QueuePacket(outapp);
+    safe_delete(outapp);
+}
+void Client::SendAA(int32 id, int seq,bool update){
+	int value=0;
+	SendAA_Struct* saa2 = NULL;
+	if(id==0)
+		saa2=zone->GetAAList()->aa[seq];
+	else
+		saa2=zone->FindAA(id);
+	int size=sizeof(SendAA_Struct)+sizeof(AA_Ability)*saa2->total_abilities;
+	uchar* buffer = new uchar[size];
+	SendAA_Struct* saa=(SendAA_Struct*)buffer;
+	memcpy(saa,saa2,size);
+	if(saa->spellid==0)
+		saa->spellid=0xFFFFFFFF;
+	if((value=GetAA(saa->id))){
+		saa->id+=value;
+		saa->last_id=saa->id-1;
+		saa->next_id=saa->id+1;
+		value++;
+		saa->current_level=value;
+		saa->cost2=value;
+		if(saa->type==1) //general ability
+			saa->abilities[0].increase_amt*=value;
+	}
+	APPLAYER* outapp = new APPLAYER(OP_SendAATable);
+	outapp->size=size;
+	outapp->pBuffer=(uchar*)saa;
+	QueuePacket(outapp);
+	safe_delete(outapp);
+}
+void Client::SendAAList(){
+	for(int i=0;i<zone->GetTotalAAs();i++){
+		SendAA(0,i);
+	}
+}
+int32 Client::GetAA(int32 aa_id){
+	if(!aa.aa_list)
+		return 0;
+	for(int i=0;i<MAX_AAS;i++){
+		if(aa.aa_list[i].aa_skill==aa_id)
+			return aa.aa_list[i].aa_value;
+		else if(aa.aa_list[i].aa_skill==0)
+			i=MAX_AAS;
+	}
+	return 0;
+}
+bool Client::SetAA(int32 aa_id, int32 new_value){
+	if(!aa.aa_list)
+		return false;
+	for(int cur=0;cur<MAX_AAS;cur++){
+		if(aa.aa_list[cur].aa_skill==aa_id){
+			aa.aa_list[cur].aa_value=new_value;
+			return true;
+		}
+		else if(aa.aa_list[cur].aa_skill==0){ //end of list
+			aa.aa_list[cur].aa_skill=aa_id;
+			aa.aa_list[cur].aa_value=new_value;
+			return true;
+		}
+	}
+	return false;
+}
+SendAA_Struct* Zone::FindAA(int32 id){
+	SendAA_Struct* ret=NULL;
+	for(int i=0;i<totalAAs;i++){
+		if(aas->aa[i]->id==id){
+			ret=aas->aa[i];
+			i=totalAAs;
+		}
+	}
+	return ret;
+}
+void Zone::LoadAAs(){
+	int32 size=database.GetSizeAA();
+	if(size>=sizeof(SendAA_Struct)){
+		aa_buffer = new uchar[size];
+		aas=(AA_List*)aa_buffer;
+		database.LoadAAs(aas);
+		totalAAs=database.CountAAs();
+	}
+	else{
+		LogFile->write(EQEMuLog::Error, "Failed to load AAs!");
+		aas=NULL;
+	}
+}
---
+++ Source/zone/AA.h
@@ -0,0 +1,73 @@
+#define MAX_AAS 120
+#define MANA_BURN 664
+#define Innate_Fire_Protection 37
+#define Innate_Cold_Protection 42
+#define Innate_Magic_Protection 47
+#define Innate_Poison_Protection 52
+#define Innate_Disease_Protection 57
+
+
+
+struct AltAdvStats_Struct {
+/*000*/  uint32 experience;
+/*004*/  uint16 unspent;
+/*006*/  uint16	unknown006;
+/*008*/  int8	percentage;
+/*009*/  int8	unknown009[3];
+};
+
+struct UseAA_Struct {
+	int32 begin;
+	int32 ability;
+	int32 end;
+};
+struct AA_Ability {
+/*00*/	int32 skill_id;
+/*04*/	int32 increase_amt;
+/*08*/	int32 unknown08;
+/*12*/	int32 last_level;
+};
+struct SendAA_Struct {
+/*0000*/	int32 id;
+/*0004*/	int32 hotkey_sid;
+/*0008*/	int32 hotkey_sid2;
+/*0012*/	int32 title_sid;
+/*0016*/	int32 desc_sid;
+/*0020*/	int32 class_type;
+/*0024*/	int32 cost;
+/*0028*/	int32 seq;
+/*0032*/	int32 current_level; //1s
+/*0036*/	int32 prereq_skill;
+/*0040*/	int32 prereq_minpoints; //min points in the prereq
+/*0044*/	int32 type;
+/*0048*/	int32 spellid;
+/*0052*/	int32 spell_type;
+/*0056*/	int32 spell_refresh;
+/*0060*/	int16 classes;
+/*0062*/	int16 berserker; //seems to be 1 if its a berserker ability
+/*0064*/	int32 max_level;
+/*0068*/	int32 last_id;
+/*0072*/	int32 next_id;
+/*0076*/	int32 cost2;
+/*0080*/	int32 unknown80; //0s
+/*0084*/	int32 total_abilities;
+/*0088*/	AA_Ability abilities[0];
+};
+struct AA_List{
+	SendAA_Struct* aa[0];
+};
+struct AA_Action{
+/*00*/	int32	action;
+/*04*/	int32	ability;
+/*08*/	int32	unknown08[2];
+};
+//  New Alternate Advancement table.  holds all the skill levels for the AA skills.
+//  Length: 309 Bytes
+//  OpCode: 1422
+struct AA_Skills{
+/*00*/	int32	aa_skill; 
+/*04*/	int32	aa_value;
+};
+struct PlayerAA_Struct {
+	AA_Skills aa_list[MAX_AAS];
+};
