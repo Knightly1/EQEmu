a56937b66715e2fce067c6df90e954c0e68434e5
fathernitwit <fathernitwit>
2005-02-28 01:02:38 +0000

    Automated Update (head)

--- Source/common/Condition.cpp
+++ Source/common/Condition.cpp
@@ -1,10 +1,58 @@
+
+#include "debug.h"
 #include "Condition.h"
+
+#ifdef WIN32
+#else
 #include <pthread.h>
 #include <sys/time.h>
 #include <errno.h>
-#include <iostream>
+#endif
 
 using namespace std;
+
+#ifdef WIN32
+/*
+
+	Windows does not support condition variables by default.
+	So we use a simple hack of sleeping in wait() and doing
+	nothing anywhere else. 
+	
+	some possible places to look for ways to do this:
+	http://www.cs.wustl.edu/~schmidt/win32-cv-1.html
+	
+	http://sources.redhat.com/pthreads-win32/
+	http://sources.redhat.com/cgi-bin/cvsweb.cgi/pthreads/pthread_cond_signal.c?rev=1.7&content-type=text/x-cvsweb-markup&cvsroot=pthreads-win32
+	
+*/
+
+#define CONDITION_HACK_GRANULARITY 4
+
+
+Condition::Condition() 
+{
+}
+
+void Condition::Signal()
+{
+}
+
+void Condition::SignalAll()
+{
+}
+
+void Condition::Wait()
+{
+	Sleep(CONDITION_HACK_GRANULARITY);
+}
+
+Condition::~Condition()
+{
+}
+
+
+#else	//!WIN32
+
 Condition::Condition() 
 {
 	pthread_cond_init(&cond,NULL);
@@ -32,6 +80,12 @@ void Condition::Wait()
 	pthread_mutex_unlock(&mutex);
 }
 
+/*
+I commented this specifically because I think it might be very
+difficult to write a windows counterpart to it, so I would like
+to discourage its use until we can confirm that it can be reasonably
+implemented on windows.
+
 bool Condition::TimedWait(unsigned long usec)
 {
 struct timeval now;
@@ -49,6 +103,7 @@ int retcode=0;
 
 	return retcode!=ETIMEDOUT;
 }
+*/
 
 Condition::~Condition()
 {
@@ -57,3 +112,5 @@ Condition::~Condition()
 	pthread_mutex_unlock(&mutex);
 	pthread_mutex_destroy(&mutex);
 }
+
+#endif
--- Source/common/Condition.h
+++ Source/common/Condition.h
@@ -1,20 +1,28 @@
 #ifndef __CONDITION_H
-
 #define __CONDITION_H
 
+#ifndef WIN32
 #include <pthread.h>
+#endif
+
+//Sombody, someday needs to figure out how to implement a condition
+//system on windows...
+
 
 class Condition {
 	private:
+#ifndef WIN32
 		pthread_cond_t cond;
 		pthread_mutex_t mutex;
+#endif
 	public:
 		Condition();
 		void Signal();
 		void SignalAll();
 		void Wait();
-		bool TimedWait(unsigned long usec);
+//		bool TimedWait(unsigned long usec);
 		~Condition();
 };
 
 #endif
+
