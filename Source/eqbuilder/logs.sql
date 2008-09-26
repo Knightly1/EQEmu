da5853f87e7002fc636ffbca06b099c14d1c2203
cavedude00 <cavedude00@projecteqemu.googlecode.com>
2008-09-26 03:29:32 +0000

    SVN r2

--- Source/eqbuilder/logs.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `logs` (
-`zone` varchar(16) NOT NULL default '',
-`name` varchar(128) NOT NULL default '',
-`type` int(10) unsigned NOT NULL default '0',
-KEY `zone` (`zone`),
-KEY `name` (`name`)
-) TYPE=MyISAM;
